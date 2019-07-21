a53212c8ed6be6fbda9acc5c4abb2c9c46cd41ae
jhw123 <hyoungwookjin123@gmail.com>
2019-07-21 23:48:43 +0900

    change homepage url in package.json

---
+++ build/precache-manifest.8af9ceb0637783ca50f54ec09210ff78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac565ab40ae0a11329d618f2d16dec9",
+    "url": "/Link-Link/index.html"
+  },
+  {
+    "revision": "657afdfa62cafbb82b7d",
+    "url": "/Link-Link/static/css/main.bf7b0c86.chunk.css"
+  },
+  {
+    "revision": "51d22fe43aefdede265f",
+    "url": "/Link-Link/static/js/2.c235fefd.chunk.js"
+  },
+  {
+    "revision": "657afdfa62cafbb82b7d",
+    "url": "/Link-Link/static/js/main.673257b9.chunk.js"
+  },
+  {
+    "revision": "93bfc147e5d1930be773",
+    "url": "/Link-Link/static/js/runtime~main.fe30846e.js"
+  }
+]);
