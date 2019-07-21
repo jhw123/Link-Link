ef67e5d18d73600b3b666aca9ff7fd92080d6ef2
jhw123 <hyoungwookjin123@gmail.com>
2019-07-21 13:01:53 +0900

    move ControlPanel to center

--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -21,6 +21,7 @@ class ControlPanel extends React.Component{
     render(){
         return(
             <div
+                className="ControlPanel"
                 onKeyDown={(e) => this.onKeyPressed(e)}
             >
                 <button
