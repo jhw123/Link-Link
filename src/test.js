917132a798ffebc3ce024ee1de4458464c15b5e4
jhw123 <hyoungwookjin123@gmail.com>
2019-07-21 12:02:25 +0900

    add color to test printouts

--- src/test.js
+++ src/test.js
@@ -3,13 +3,24 @@ const RIGHT = 1;
 const DOWN = 2;
 const LEFT = 3;
 
-function test(test_name, expectation, test){
-    if(expectation === test)
-        console.log("PASS");
+let TEST_NUM = 1;
+
+const TERMINAL_RESET = '\x1b[0m';
+const TERMINAL_RED = '\x1b[31m';
+const TERMINAL_GREEN = '\x1b[32m';
+const TERMINAL_YELLOW = '\x1b[33m';
+const TERMINAL_CYAN = '\x1b[36m';
+const TERMINAL_BLUE = '\x1b[34m';
+
+function test(test_name, expectation, func, args){
+    const test_val = func(...args);
+    if(expectation === test_val)
+        console.log(`${TEST_NUM}. ${TERMINAL_GREEN}PASS${TERMINAL_RESET} - ${TERMINAL_YELLOW}${func.name}${TERMINAL_RESET}`);
     else{
-        console.log(`FAIL - ${test_name}`);
-        console.log(`Expect: ${expectation} but got: ${test}`);
+        console.log(`${TEST_NUM}. ${TERMINAL_RED}FAIL${TERMINAL_RESET} - ${TERMINAL_YELLOW}${func.name}${TERMINAL_RESET} - ${test_name}`);
+        console.log(`${TERMINAL_CYAN}Expect:${TERMINAL_RESET} ${expectation} ${TERMINAL_CYAN}but got:${TERMINAL_RESET} ${test_val}`);
     }
+    TEST_NUM++;
 }
 
 function checkLinked(check_block, adjacent_block, direction){
@@ -24,8 +35,22 @@ function checkLinked(check_block, adjacent_block, direction){
     return false;
 }
 
-test("true case", true, checkLinked({pattern: [UP], color: 'red'}, {pattern: [DOWN, UP], color: 'red'}, UP));
-test("different color", false, checkLinked({pattern: [UP], color: 'red'}, {pattern: [DOWN, UP], color: 'yello'}, UP));
-test("check_block has no link", false, checkLinked({pattern: [DOWN], color: 'red'}, {pattern: [DOWN, UP], color: 'red'}, UP));
-test("adjacent has no link", false, checkLinked({pattern: [UP], color: 'red'}, {pattern: [UP], color: 'red'}, UP));
-test("no adjacent block", false, checkLinked({pattern: [UP], color: 'red'}, false, UP));
+function checkArrayIn2DArray(target_array, check_array){
+    for(let cur_array of target_array){
+        if(cur_array[0] === check_array[0] && cur_array[1] === check_array[1])
+            return true;
+    }
+    return false;
+}
+
+test("true case for UP", true, checkLinked, [{pattern: [UP], color: 'red'}, {pattern: [DOWN, UP], color: 'red'}, UP]);
+test("true case for DOWN", true, checkLinked, [{pattern: [DOWN], color: 'red'}, {pattern: [DOWN, UP], color: 'red'}, DOWN]);
+test("true case for LEFT", true, checkLinked, [{pattern: [LEFT, RIGHT], color: 'red'}, {pattern: [RIGHT, DOWN, UP], color: 'red'}, LEFT]);
+test("true case for RIGHT", true, checkLinked, [{pattern: [RIGHT, UP, LEFT], color: 'red'}, {pattern: [LEFT, DOWN, UP], color: 'red'}, RIGHT]);
+test("different color", false, checkLinked, [{pattern: [UP], color: 'red'}, {pattern: [DOWN, UP], color: 'yello'}, UP]);
+test("check_block has no link", false, checkLinked, [{pattern: [DOWN], color: 'red'}, {pattern: [DOWN, UP], color: 'red'}, UP]);
+test("adjacent has no link", false, checkLinked, [{pattern: [UP], color: 'red'}, {pattern: [UP], color: 'red'}, UP]);
+test("no adjacent block", false, checkLinked, [{pattern: [UP], color: 'red'}, false, UP]);
+
+test("these is a duplicate", true, checkArrayIn2DArray, [[[8, 0],[9, 0],[8, 1],[9, 1]], [9,1]]);
+test("these is no duplicate", false, checkArrayIn2DArray, [[[8, 0],[9, 0],[8, 1]], [9,1]]);
