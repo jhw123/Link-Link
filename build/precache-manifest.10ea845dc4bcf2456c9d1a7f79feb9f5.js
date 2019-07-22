d2204c2c621aa4957cceba5283bba9954c02ad4f
jhw123 <hyoungwookjin123@gmail.com>
2019-07-22 18:50:15 +0900

    build game ver. 2.0

---
+++ build/precache-manifest.10ea845dc4bcf2456c9d1a7f79feb9f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7295839fa0219d0e7fb370f1c3f1c726",
+    "url": "/Link-Link/index.html"
+  },
+  {
+    "revision": "9a1c6ba7b028c9f9444c",
+    "url": "/Link-Link/static/css/main.25b453c6.chunk.css"
+  },
+  {
+    "revision": "3d8601e616d677fef21d",
+    "url": "/Link-Link/static/js/2.e125c52e.chunk.js"
+  },
+  {
+    "revision": "9a1c6ba7b028c9f9444c",
+    "url": "/Link-Link/static/js/main.6c14ed17.chunk.js"
+  },
+  {
+    "revision": "93bfc147e5d1930be773",
+    "url": "/Link-Link/static/js/runtime~main.fe30846e.js"
+  }
+]);
